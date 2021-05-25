62bc221f57d0e18408830747239a13f5b3f5095a
xubowenW <416074318@qq.com>
2021-05-25 16:58:27 +0800

    @ConfigurationProperties

--- src/main/java/org/example/MainApplication.java
+++ src/main/java/org/example/MainApplication.java
@@ -8,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+//点开springbootapplication
+/**
+ * springbootapplication中包含以下几个注解
+ * @SpringBootConfiguration 代表当前是一个配置类
+ * @EnableAutoConfiguration
+ * @ComponentScan 指定扫描哪些内容
+ *
+ * @EnableAutoConfiguration注解是以下几个注解的合成注解：
+ * @AutoConfigurationPackage 指定了默认的包规则
+ * @Import({AutoConfigurationImportSelector.class}) 将指定的包下所有的组件导入进来，MainApplication所在的包
+ *
+ */
 public class MainApplication
 {
     public static void main( String[] args )
